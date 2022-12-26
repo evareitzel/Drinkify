5a7cb13b8c6949b72a075c63c765037ff78ebdd2
Eva Reitzel <evareitzel@gmail.com>
2022-12-26 15:18:28 -0500

    seeds.rb pseudocode

--- app-server/app/controllers/application_controller.rb
+++ app-server/app/controllers/application_controller.rb
@@ -7,7 +7,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/drinks" do
+     # get all the drinks from the database
+     drinks = Drink.all.order(:name).limit(6)
+    # return a JSON response with an array of all the drink data
+    drinks.to_json
+
     { message: "Drinkify recipes from https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a" }.to_json
+  end
+    # look up the game in the database using its ID
+    # send a JSON-formatted response of the game data
+  get '/drinks/:name' do
+    drink = Drink.find_by(params[:name]) # check
+    drink.to_json
   end
 
 end
--- app-server/db/seeds.rb
+++ app-server/db/seeds.rb
@@ -2,4 +2,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+# # Make 2 mixologists
+# 2.times do
+#   Mixologist.create(name: Faker::Name.name)
+# end 
+
+# # Make 6 drinks
+# 6.times do
+#   drink = Drink.create(
+#     name
+#   )
+
+
 puts "✅ Done seeding!"
