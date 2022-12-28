9f4a9c4de022b1117b061013ed0aad7279a03e2a
Eva Reitzel <evareitzel@gmail.com>
2022-12-28 14:16:48 -0500

    /app-client: Add components folder, place App.js inside. Delete app.css, logo.svg, App.test.js, setupTests.js

--- note.rb
+++ note.rb
@@ -142,4 +142,11 @@ end
 # data = GetRequester.new
 # p data.parse_json
 
+# from setupTests.js:
+# // jest-dom adds custom jest matchers for asserting on DOM nodes.
+# // allows you to do things like:
+# // expect(element).toHaveTextContent(/react/i)
+# // learn more: https://github.com/testing-library/jest-dom
+# import '@testing-library/jest-dom';
+
 🥂🍻 🍸
