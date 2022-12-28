f4841f7fed33cef668edca638e46a6425d425e21
Eva Reitzel <evareitzel@gmail.com>
2022-12-28 15:05:13 -0500

    Edit App.js

--- app-client/src/components/App.js
+++ app-client/src/components/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./Header";//unghost
+import Search from "./Search";
+import DrinkList from "./DrinkList";
+import NewDrink from "./NewDrink";
 
 function App() {
+  // darkmode
+  const [drinks, setDrinks] = useState([]);
+  // const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:9292/drinks")
+    .then(r => r.json()) // (r)
+    .then(drinks => setDrinks(drinks)); // (drinks)
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    <Header />
+    <Search />
+    <DrinkList />
+    <NewDrink />
+        // <p>
+        //   Edit <code>src/App.js</code> and save to reload.
+        // </p>
+        // <a
+        //   className="App-link"
+        //   href="https://reactjs.org"
+        //   target="_blank"
+        //   rel="noopener noreferrer"
+        // >
+        //   Learn React
+        // </a>
+    //   </header>
+    // </div>
   );
 }
 
