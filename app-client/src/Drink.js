4d278e5a514cc77e7f70226e53a7d1cb0e5a7ee6
Eva Reitzel <evareitzel@gmail.com>
2022-12-30 10:21:22 -0500

    Pass drink obj from DrinkList to Drink; deconstruct drink info in Drink component

--- app-client/src/Drink.js
+++ app-client/src/Drink.js
@@ -1,5 +1,6 @@
-export default function Drink({name, glass, instructions_en, ingredient1, ingredient2, ingredient3, measure1, measure2
-}){
+export default function Drink({drink}){
+  const {name, glass, instructions_en, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, measure1, measure2, measure3, measure4, measure5} = drink
+  
   return(
     <div className="Card">
       <h2>{name}</h2>
@@ -7,8 +8,10 @@ export default function Drink({name, glass, instructions_en, ingredient1, ingred
       <h3>Ingredients</h3>
       <ul>
         <li>{measure1} {ingredient1}</li>
-        <li>{measure2}{ingredient2}</li>
-        <li>{ingredient3}</li>
+        <li>{measure2} {ingredient2}</li>
+        <li>{measure3} {ingredient3}</li>
+        <li>{measure4} {ingredient4}</li>
+        <li>{measure5} {ingredient5}</li>
       </ul>
       <p>{instructions_en}</p>
     </div>
--- app-client/src/DrinkList.js
+++ app-client/src/DrinkList.js
@@ -2,23 +2,12 @@ import Drink from "./Drink";
 
 export default function DrinkList({drinks}){
   console.log(drinks)
-  // an I pass keys as "drink" and deconstruct within Drink component?
-  // Add Measure 2, 3, 4
   return(
     <div> {/* className="DrinkList" */}
       <h2>Drinks</h2>
       {/* <ul> */}
         {drinks.map(drink => (
-          <Drink 
-            key={drink.id}
-            name={drink.name}
-            glass={drink.glass}
-            instructions_en={drink.instructions_en}
-            ingredient1={drink.ingredient1}
-            ingredient2={drink.ingredient2}
-            ingredient3={drink.ingredient3}
-            measure1={drink.measure1}
-          />
+          <Drink key={drink.id} drink = {drink} />
         ))}
       {/* </ul> */}
     </div>
